a489cc1accfd59fad3a7385a8fb8fdf4b7ebbdd6
daltkb <daltbenn@gmail.com>
2022-09-15 20:08:05 -0500

    Header and about section

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,21 @@ import type { Component } from 'solid-js';
 
 const App: Component = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello</h1>
-    </div>
+    <>
+      <div class="mt-16 flex flex-col">
+        <h1 class="text-5xl font-bold ">Dalton</h1>
+        <h1 class="text-5xl font-bold">Bennyhoff</h1>
+        <h2 class="text-2xl pt-2">Software Engineer</h2>
+      </div>
+      <div class="mt-14">
+        <h2 class="text-xl py-2 font-bold">About</h2>
+        <p>
+          Hi I'm Dalton. I'm a software engineer based out of Dallas, TX. Before
+          I started writing code; I was a pro-gamer in the Overwatch League. In
+          my spare time I enjoy watching hockey and supporting Liverpool FC.
+        </p>
+      </div>
+    </>
   );
 };
 
